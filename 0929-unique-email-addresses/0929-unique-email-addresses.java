b500bf4ca21ac9a1d537a6dc7068a1690df9badc
mahesh01mahesh <vtu25250@veltech.edu.in>
2026-04-21 12:19:24 +0530

    Time: 15 ms (27.4%), Space: 47.2 MB (22.73%) - LeetHub

---
+++ 0929-unique-email-addresses/0929-unique-email-addresses.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> set = new HashSet<>();
+        
+        for (String email : emails) {
+            String[] parts = email.split("@");
+            String local = parts[0];
+            String domain = parts[1];
+            
+            StringBuilder sb = new StringBuilder();
+            
+            for (char c : local.toCharArray()) {
+                if (c == '+') break;
+                if (c != '.') sb.append(c);
+            }
+            
+            set.add(sb.toString() + "@" + domain);
+        }
+        
+        return set.size();
+    }
+}
